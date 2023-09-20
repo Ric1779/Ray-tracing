d3860924866ea69a0630fd649baaa17d5b54d4c8
Richards <richards.1779@gmail.com>
2023-09-20 02:25:35 +0200

    Diffuse Material : Stage 1

--- sphere.h
+++ sphere.h
@@ -12,7 +12,7 @@ class sphere: public hittable{
     public:
         sphere(point3 _center, double _radius) : radius(_radius), center(_center) {}
 
-        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc = r.origin() - center;
             auto a = r.direction().length_squared();
             auto half_b = dot(oc, r.direction());
@@ -24,9 +24,9 @@ class sphere: public hittable{
 
             // find the nearest root that lies in the acceptable range.
             auto root = (-half_b - sqrtd)/a;
-            if (root <= ray_tmin || root >= ray_tmax){
+            if (!ray_t.surrounds(root)){
                 root = (-half_b + sqrtd)/a;
-                if(root <= ray_tmin || root >= ray_tmax)
+                if(!ray_t.surrounds(root))
                     return false;
             }
 
