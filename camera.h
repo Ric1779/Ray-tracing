af1a8a71bcb7393aae4fc8380651a023321f48c9
Richards <richards.1779@gmail.com>
2023-09-20 15:43:39 +0200

    Diffuse Material : Gamma Correction

--- camera.h
+++ camera.h
@@ -93,9 +93,10 @@ class camera{
             if (depth <= 0)
                 return color(0,0,0);
 
-            if (world.hit(r, interval(0, infinity), rec)) {
-                vec3 direction = random_on_hemisphere(rec.normal);
-                return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+            if (world.hit(r, interval(0.001, infinity), rec)) {
+                //vec3 direction = random_on_hemisphere(rec.normal);
+                vec3 direction = rec.normal + random_unit_vector();
+                return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);//Then shouldn't the value always be zero
             }
 
             vec3 unit_direction = unit_vector(r.direction());
