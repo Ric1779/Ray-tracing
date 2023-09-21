1f9295ec5120bf54816af3f30ac1d0cccb09ca74
Richards <richards.1779@gmail.com>
2023-09-21 02:36:36 +0200

    Material class added, main and camera updated

--- sphere.h
+++ sphere.h
@@ -11,8 +11,8 @@ class sphere: public hittable{
         shared_ptr<material> mat;  // material class hasn't been included and it still works WHY?
 
     public:
-        sphere(point3 _center, double _radius, shared_ptr<material> _material) 
-        : radius(_radius), center(_center), mat(_material) {}
+        sphere(point3 _center, double _radius, shared_ptr<material> _material)
+        : center(_center), radius(_radius), mat(_material) {}
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc = r.origin() - center;
@@ -36,6 +36,7 @@ class sphere: public hittable{
             rec.p = r.at(rec.t);
             vec3 outward_normal = (rec.p - center) / radius;
             rec.set_face_normal(r, outward_normal);
+            rec.mat = mat;
 
             return true;
         }
