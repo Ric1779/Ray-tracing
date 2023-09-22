1ca660d863270d6aa74e67fe64a8e8f76bca367a
Richards <richards.1779@gmail.com>
2023-09-22 19:05:50 +0200

    Final Render

--- material.h
+++ material.h
@@ -37,19 +37,20 @@ class lambertian : public material {
 };
 
 class metal : public material {
-    private:
-        color albedo;
+  public:
+    metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
-    public:
-        metal(const color& a) : albedo(a) {}
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+        attenuation = albedo;
+        return (dot(scattered.direction(), rec.normal) > 0);
+    }
 
-        bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
-        const override {
-            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
-            attenuation = albedo;
-            return true;
-        }
+  private:
+    color albedo;
+    double fuzz;
 };
 
 class dielectric : public material {
