61349bc71dce2b2f601d9a0dab3112b37a674516
Richards <richards.1779@gmail.com>
2023-10-04 20:42:02 +0200

    Motion Blur

--- sphere.h
+++ sphere.h
@@ -6,15 +6,34 @@
 
 class sphere: public hittable{
     private:
+        point3 center1;
         double radius;
         point3 center;
         shared_ptr<material> mat;  // material class hasn't been included and it still works WHY?
+        bool is_moving;
+        vec3 center_vec;
+
+        point3 sphere_center(double time) const {
+            // Linearly interpolate from center1 to center2 according to time, where t=0 yields
+            // center 1 and t=1 yields center2
+
+            return center1 + time*center_vec;
+        }
 
     public:
+        // Static sphere
         sphere(point3 _center, double _radius, shared_ptr<material> _material)
-        : center(_center), radius(_radius), mat(_material) {}
+        : center1(_center), radius(_radius), mat(_material), is_moving(false) {}
+        
+        // Moving sphere
+        sphere(point3 _center1, point3 _center2, double _radius, shared_ptr<material> _material)
+        : center1(_center1), radius(_radius), mat(_material), is_moving(true)
+        { 
+            center_vec = _center2 - _center1; 
+        }
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+            point3 center = is_moving ? sphere_center(r.time()) : center1;
             vec3 oc = r.origin() - center;
             auto a = r.direction().length_squared();
             auto half_b = dot(oc, r.direction());
