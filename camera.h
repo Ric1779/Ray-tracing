56eb5960877706fd3758397d2660ebb69789acc3
Richards <richards.1779@gmail.com>
2023-11-12 00:18:31 +0100

    Part 2 Final Commit!!

--- camera.h
+++ camera.h
@@ -36,7 +36,7 @@ class camera{
                     color pixel_color(0,0,0);
                     for (int sample = 0; sample < samples_per_pixel; ++sample) {
                         ray r = get_ray(i, j);
-                        pixel_color += ray_color(r, max_depth, world);
+                        pixel_color += ray_color_old(r, max_depth, world);
                     }
                     write_color(std::cout, pixel_color, samples_per_pixel);
                 }
@@ -118,6 +118,26 @@ class camera{
             // Returns a random point in the camera defocus disk. 'Performs defocus blur or depth of field'
             auto p = random_in_unit_disk();
             return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+        }
+
+        color ray_color_old(const ray& r, int depth, const hittable& world) const {
+            hit_record rec;
+
+            // If we've exceeded the ray bounce limit, no more light is gathered.
+            if (depth <= 0)
+                return color(0,0,0);
+
+            if (world.hit(r, interval(0.001, infinity), rec)) {
+                ray scattered;
+                color attenuation;
+                if (rec.mat->scatter(r, rec, attenuation, scattered))
+                    return attenuation * ray_color_old(scattered, depth-1, world);
+                return color(0,0,0);
+            }
+
+            vec3 unit_direction = unit_vector(r.direction());
+            auto a = 0.5*(unit_direction.y() + 1.0);
+            return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
         }
 
         color ray_color(const ray& r, int depth, const hittable& world) const {
--- material.h
+++ material.h
@@ -98,7 +98,7 @@ class dielectric : public material {
     }
 };
 
-class diffuse_light: public material {
+class diffuse_light : public material {
     public:
         diffuse_light(shared_ptr<texture> a) : emit(a) {}
         diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
@@ -112,6 +112,22 @@ class diffuse_light: public material {
         }
     private:
         shared_ptr<texture> emit;
+};
+
+class isotropic : public material {
+  public:
+    isotropic(color c) : albedo(make_shared<solid_color>(c)) {}
+    isotropic(shared_ptr<texture> a) : albedo(a) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        scattered = ray(rec.p, random_unit_vector(), r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        return true;
+    }
+
+  private:
+    shared_ptr<texture> albedo;
 };
 
 #endif
